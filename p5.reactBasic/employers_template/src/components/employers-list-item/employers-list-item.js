e3284189eb37cbad83c0cbe2ce7f5199ef773dc7
Nurzhan Zeinolla <zeinolla.nur04@gmail.com>
2024-08-03 13:10:20 +0500

    feat: added method onRise

--- p5.reactBasic/employers_template/src/components/employers-list-item/employers-list-item.js
+++ p5.reactBasic/employers_template/src/components/employers-list-item/employers-list-item.js
@@ -8,7 +8,7 @@ class EmployersListItem extends Component {
     super(props);
     this.state = {
       increase: false,
-
+      rise: false
     }
   }
 
@@ -18,18 +18,27 @@ class EmployersListItem extends Component {
     }));
   }
 
+  onRise = () => {
+    this.setState(({rise}) => ({
+      rise: !rise
+    }))
+  }
+
   render() {
     const {name, salary} = this.props;
-    const {increase} = this.state;
+    const {increase, rise} = this.state;
 
       let classNames = "list-group-item d-flex justify-content-between";
       if (increase) {
         classNames += " increase"
       }
+      if (rise) {
+        classNames += " like"
+      }
 
       return (
         <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
