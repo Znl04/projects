ffdf88f8aae384d4e6927efd0beb3cd8175afe6c
Nurzhan Zeinolla <zeinolla.nur04@gmail.com>
2024-11-27 21:03:13 +0500

    refac: ready for work

--- p6.journalOfIncomes/journal-of-incomes/src/index.js
+++ p6.journalOfIncomes/journal-of-incomes/src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const elem = (
+  <div>
+    <h1>Контролируйте свой доход!</h1>
+
+    <div>
+      <h2>Сколько сегодня вы потратили?</h2>
+      <input type="number"/>
+      <button>Клик</button>
+    </div>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
