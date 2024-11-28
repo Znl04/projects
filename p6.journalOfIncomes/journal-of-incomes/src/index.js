3ed1978a76bb507b69af0e64eda7e6e700ac412e
Nurzhan Zeinolla <zeinolla.nur04@gmail.com>
2024-11-28 11:37:30 +0500

    feat: components and elements

--- p6.journalOfIncomes/journal-of-incomes/src/index.js
+++ p6.journalOfIncomes/journal-of-incomes/src/index.js
@@ -3,19 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = (
-  <div>
-    <h1>Контролируйте свой доход!</h1>
-
-    <div>
-      <h2>Сколько сегодня вы потратили?</h2>
-      <input type="number"/>
-      <button>Клик</button>
-    </div>
-  </div>
-);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App></App>
 );
