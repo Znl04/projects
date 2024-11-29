1d1a047be3e42015f5ed0c517232f8895da772bb
Nurzhan Zeinolla <zeinolla.nur04@gmail.com>
2024-11-30 00:52:11 +0500

    feat: structured and rendered in index js

--- p6.journalOfIncomes/journal-of-incomes/src/Components/app/app.js
+++ p6.journalOfIncomes/journal-of-incomes/src/Components/app/app.js
@@ -1,10 +1,16 @@
 import AppInfo from "../app-info/app-info";
+import AppFiler from "../app-filter/app-filter";
+import IncomesList from "../incomes-list/incomes-list";
+import IncomesAddForm from "../incomes-add-form/incomes-add-form";
 import "./app.css";
 
 function App() {
   return (
     <div className="app">
       <AppInfo/>
+      <AppFiler/>
+      <IncomesList/>
+      <IncomesAddForm/>
     </div>
   );
 }
