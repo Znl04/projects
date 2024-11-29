4f668070332aa427b2a7bac4bd4e7a728d127350
Nurzhan Zeinolla <zeinolla.nur04@gmail.com>
2024-11-29 11:07:22 +0500

    feat: added first component and structured my components folder

---
+++ p6.journalOfIncomes/journal-of-incomes/src/Components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Контролируйте свой доход!</h1>
+      <h2>Всего потрачено: </h2>
+      <h2>Всего доходов: </h2>
+    </div>
+  );
+}
+
+export default AppInfo;
